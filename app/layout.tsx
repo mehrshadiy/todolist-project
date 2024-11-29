df89e023d6d022aaac9740a162a9a4af9c93bef5
mehrshadiy <mehrshadshakery@yahoo.com>
2024-11-29 18:02:04 +0330

    implement header and hero styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
 import "./globals.css";
-
-
+import React from "react";
 
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body>
+        <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: `url('/mountain.png')`, backgroundPosition: '0% 65%',zIndex: '-1'}}>
+            <div
+            style={{width:'100%', height:'60%', background: 'linear-gradient(225deg, #5596FF 0%, #AC2DEB 100%)', opacity: '70%', zIndex: '-1', position: 'absolute'}}
+            />
+            <div style={{width: '100%', height: '40%', position: 'absolute', zIndex: '-1', left: 0, bottom: 0, backgroundColor: 'white'}}/>
+        </div>
+        <div style={{zIndex: '1', position: 'relative', paddingTop:"75px", paddingBottom: '55px', maxWidth: '540px', margin: '0 auto'}}>
+            {children}
+        </div>
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,34 @@
+import {FaMoon} from "react-icons/fa";
+import {PiCircleThin} from "react-icons/pi";
+
 export default function Home() {
-  return (
-    <main>
-      <div className=" relative bg-neutral-50">
-  <div className="">
-    <div className="bg-gradient-to-bl from-[#5495ff] to-[#ac2dea]" />
-    <div className="bg-gradient-to-bl from-[#5495ff] to-[#ac2dea]" />
-    <Image className="opacity-25"  src="https://via.placeholder.com/1440x960" width={} height={}/>
-  </div>
-  <div className="">
-    <div className="bg-white rounded-[5px] shadow" />
-    <div className="justify-center items-center gap-6 inline-flex">
-      <div className="bg-white rounded-full border border-[#e3e3f0]" />
-      <div className="text-[#9395a5] text-lg font-normal font-['Josefin Sans']">Create a new todo…</div>
-    </div>
-  </div>
-  <div className="">
-    <div className="bg-white rounded-[5px] shadow" />
-    <div className="left-[24px] top-[405px] absolute text-[#9395a5] text-sm font-normal font-['Josefin Sans']">5 items left</div>
-    <div className="left-[411px] top-[405px] absolute text-right text-[#9395a5] text-sm font-normal font-['Josefin Sans']">Clear Completed</div>
-    <div className="pb-1 left-[189px] top-[405px] absolute justify-center items-start gap-[19px] inline-flex">
-      <div className="text-[#397cfc] text-sm font-bold font-['Josefin Sans']">All</div>
-      <div className="text-[#9395a5] text-sm font-bold font-['Josefin Sans']">Active</div>
-      <div className="text-[#9395a5] text-sm font-bold font-['Josefin Sans']">Completed</div>
-    </div>
-    <div className="bg-[#e3e3f0]" />
-    <div className="">
-      <div className="">
-        <div className="bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="">
-          <div className="bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <Image className="" src="https://via.placeholder.com/18x18" />
-      <div className="left-[48px] top-[1px] absolute text-[#d1d2da] text-lg font-normal font-['Josefin Sans'] line-through">Complete online JavaScript course</div>
-    </div>
-    <div className="left-[24px] top-[84px] absolute justify-center items-start gap-6 inline-flex">
-      <div className="">
-        <div className="bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="opacity-0">
-          <div className="bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <div className="text-[#484c6b] text-lg font-normal font-['Josefin Sans']">Jog around the park 3x</div>
-    </div>
-    <div className="bg-[#e3e3f0]" />
-    <div className="">
-      <div className="">
-        <div className=" bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="opacity-0 ">
-          <div className=" bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <Image className="" src="https://via.placeholder.com/18x18" />
-      <div className=" text-[#484c6b] text-lg font-normal font-['Josefin Sans']">10 minutes meditation</div>
-    </div>
-    <div className=" bg-[#e3e3f0]" />
-    <div className="">
-      <div className="">
-        <div className=" bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="opacity-0">
-          <div className=" bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <Image className="" src="https://via.placeholder.com/18x18" />
-      <div className="left-[48px] top-[1px] absolute text-[#484c6b] text-lg font-normal font-['Josefin Sans']">Read for 1 hour</div>
-    </div>
-    <div className=" bg-[#e3e3f0]" />
-    <div className="">
-      <div className="">
-        <div className=" bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="opacity-0">
-          <div className=" bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <Image className="" src="https://via.placeholder.com/18x18" />
-      <div className=" text-[#484c6b] text-lg font-normal font-['Josefin Sans']">Pick up groceries</div>
-    </div>
-    <div className=" bg-[#e3e3f0]" />
-    <div className=" bg-[#e3e3f0]" />
-    <div className="">
-      <div className="">
-        <div className=" bg-white rounded-full border border-[#e3e3f0]" />
-        <div className="opacity-0 ">
-          <div className=" bg-gradient-to-br from-[#54dcff] to-[#bf57f3] rounded-full" />
-        </div>
-      </div>
-      <Image className="" src="https://via.placeholder.com/18x18" />
-      <div className=" text-[#484c6b] text-lg font-normal font-['Josefin Sans']">Complete Todo App on Frontend Mentor</div>
-    </div>
-  </div>
-  <div className="">
-    <div className=" text-white text-[40px] font-bold font-['Josefin Sans'] tracking-[15px]">TODO</div>
-    <Image className="" src="https://via.placeholder.com/25x26" />
-  </div>
-  <div className=" text-center text-[#9395a5] text-sm font-normal font-['Josefin Sans']">Drag and drop to reorder list</div>
-</div>
-    </main>
-  )
+    return (
+        <>
+            <header className={'container mb-12'}>
+                <div className={'flex justify-between items-center'}>
+                    <h1 className={`text-white text-[40px] font-bold font-['Josefin Sans'] tracking-[15px]`}>
+                        TODO
+                    </h1>
+                    <FaMoon size={26} color={'white'} />
+                </div>
+            </header>
+            <main>
+                <form action={''} className={' container bg-white px-6 py-5 flex items-center gap-6 mb-6'}>
+                    <label htmlFor="new-task">
+                        <PiCircleThin size={24} color={'gray'}/>
+                    </label>
+                    <input type={'text'} name={'new-task'} id={'new-task'} placeholder={'Create a new todo...'} className={'focus:outline-none focus:border-none text-gray-500 w-full h-full'} />
+                </form>
+            </main>
+            <footer className={'container text-gray-700 text-center gap-5 grid '}>
+                <h3>
+                    Drag and drop to reorder list
+                </h3>
+                <p>
+                    All Rights Reserved By MEHRSHADIY
+                </p>
+            </footer>
+        </>
+
+    )
 }
