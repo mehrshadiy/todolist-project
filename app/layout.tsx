701e74b2260b4a360a5b922638f39aa88a470c70
mehrshadiy <mehrshadshakery@yahoo.com>
2024-11-29 23:10:59 +0330

    implement main styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,13 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body>
-        <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundImage: `url('/mountain.png')`, backgroundPosition: '0% 65%',zIndex: '-1'}}>
+        <div style={{position: 'absolute', top: 0, left: 0, width: '100vw', height: '100vh', backgroundImage: `url('/mountain.png')`, backgroundPosition: '0% 65%', backgroundRepeat: "no-repeat", backgroundSize: 'cover',zIndex: '-1'}}>
             <div
             style={{width:'100%', height:'60%', background: 'linear-gradient(225deg, #5596FF 0%, #AC2DEB 100%)', opacity: '70%', zIndex: '-1', position: 'absolute'}}
             />
-            <div style={{width: '100%', height: '40%', position: 'absolute', zIndex: '-1', left: 0, bottom: 0, backgroundColor: 'white'}}/>
+            <div style={{width: '100%', height: '40%', position: 'absolute', zIndex: '0', left: 0, bottom: 0, backgroundColor: 'white'}}/>
         </div>
-        <div style={{zIndex: '1', position: 'relative', paddingTop:"75px", paddingBottom: '55px', maxWidth: '540px', margin: '0 auto'}}>
+        <div style={{zIndex: '2', position: 'relative', paddingTop:"75px", paddingBottom: '55px', maxWidth: '540px', margin: '0 auto'}}>
             {children}
         </div>
         </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import {FaMoon} from "react-icons/fa";
-import {PiCircleThin} from "react-icons/pi";
+import {FaCheck, FaMoon} from "react-icons/fa";
+import {PiCheckCircleThin, PiCheckThin, PiCircleThin} from "react-icons/pi";
 
 export default function Home() {
     return (
@@ -9,18 +9,52 @@ export default function Home() {
                     <h1 className={`text-white text-[40px] font-bold font-['Josefin Sans'] tracking-[15px]`}>
                         TODO
                     </h1>
-                    <FaMoon size={26} color={'white'} />
+                    <FaMoon size={26} color={'white'}/>
                 </div>
             </header>
-            <main>
-                <form action={''} className={' container bg-white px-6 py-5 flex items-center gap-6 mb-6'}>
+            <main className={'mb-12'}>
+                <form action={''} className={'container bg-white px-6 py-5 flex items-center gap-6 mb-6 rounded-lg'}>
                     <label htmlFor="new-task">
                         <PiCircleThin size={24} color={'gray'}/>
                     </label>
-                    <input type={'text'} name={'new-task'} id={'new-task'} placeholder={'Create a new todo...'} className={'focus:outline-none focus:border-none text-gray-500 w-full h-full'} />
+                    <input type={'text'} name={'new-task'} id={'new-task'} placeholder={'Create a new todo...'}
+                           className={'focus:outline-none focus:border-none text-gray-500 w-full h-full'}/>
                 </form>
+                <div
+                    className={'max-h-[439px] min-w-[540px] bg-white rounded-lg border border-gray-200 drop-shadow-2xl shadow-2xl shadow-gray-300'}>
+                    <ul className={`min-h-14 text-lg text-[#494C6B]`}>
+                        <li className={'px-6 py-5 flex items-center gap-3.5 border-b text-[#D1D2DA] line-through'}>
+                            <FaCheck size={20} color={'white'} className={'bg-gradient-to-br from-blue-400 to-purple-500 rounded-full p-1'}/>
+                            Complete online JavaScript course
+                        </li>
+                        <li className={'px-6 py-5 flex items-center gap-3.5 border-b'}>
+                            <PiCircleThin size={24} color={'gray'}/>
+                            Jog around the park 3x
+                        </li>
+                    </ul>
+                    <div className={'flex px-6 py-5 justify-between text-gray-500 text-sm'}>
+                        <p>
+                            5 items left
+                        </p>
+                        <ul className={'flex gap-5'}>
+                            <li className={"text-blue-700"}>
+                                All
+                            </li>
+                            <li>
+                                Active
+                            </li>
+                            <li>
+                                Completed
+                            </li>
+                        </ul>
+                        <button>
+                            Clear Completed
+                        </button>
+                    </div>
+                </div>
+
             </main>
-            <footer className={'container text-gray-700 text-center gap-5 grid '}>
+            <footer className={'container text-gray-800 text-center gap-5 grid'}>
                 <h3>
                     Drag and drop to reorder list
                 </h3>
